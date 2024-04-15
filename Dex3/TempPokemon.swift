ad3d9e6e2497c24cbdc9e125b73c7cfd5ff07efa
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-04-16 08:25:15 +0900

    enumを追加

--- Dex3/TempPokemon.swift
+++ Dex3/TempPokemon.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct TempPokemon: Codable {
     let id: Int
-    let Name: String
+    let name: String
     let types: [String]
     let hp: Int
     let attack: Int
@@ -19,4 +19,35 @@ struct TempPokemon: Codable {
     let speed: Int
     let sprite: URL
     let shiny: URL
+    
+    // EncodeとDecodeでキー名が異なる時に一対一対応させる
+    enum PokemonKeys: String, CodingKey {
+        case id
+        case name
+        case types
+        case stats
+        case sprites
+        
+        enum TypeDictionaryKeys: String, CodingKey {
+            case type
+            
+            enum TypeKeys: String, CodingKey {
+                case name
+            }
+        }
+        
+        enum StatDictionarykeys: String, CodingKey {
+            case value = "base_stat"
+            case stat
+            
+            enum StatKeys: String, CodingKey {
+                case name
+            }
+        }
+        
+        enum SpriteKeys: String, CodingKey {
+            case sprite = "front_default"
+            case shiny = "ront_shiny"
+        }
+    }
 }
