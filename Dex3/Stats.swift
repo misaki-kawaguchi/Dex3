7db59659fa78280b4aa284983fa0981d8de78854
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-04-22 21:37:16 +0900

    PokemonDetailにStatsを追加

---
+++ Dex3/Stats.swift
@@ -0,0 +1,37 @@
+//
+//  Stats.swift
+//  Dex3
+//
+//  Created by 川口美咲 on 2024/04/22.
+//
+
+import SwiftUI
+import Charts
+
+struct Stats: View {
+    @EnvironmentObject var pokemon: Pokemon
+    
+    var body: some View {
+        Chart(pokemon.stats) { stat in
+            BarMark(
+                x:.value("Value", stat.value),
+                y:.value("Stat", stat.label)
+            )
+            .annotation(position: .trailing) {
+                Text("\(stat.value)")
+                    .padding(.top, -5)
+                    .foregroundColor(.secondary)
+                    .font(.subheadline)
+            }
+        }
+        .frame(height: 200)
+        .padding([.leading, .bottom, .trailing])
+        .foregroundColor(Color(pokemon.types![0].capitalized))
+        .chartXScale(domain: 0...pokemon.highestStat.value+5)
+    }
+}
+
+#Preview {
+    Stats()
+        .environmentObject(SamplePokemon.samplePokemon)
+}
