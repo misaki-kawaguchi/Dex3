54c8ef7d4b0958a17f87bb6b74e54bf3e8244c92
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-04-28 18:42:11 +0900

    Widget Extensionを作成

---
+++ Dex3Widget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  Dex3Widget
+//
+//  Created by 川口美咲 on 2024/04/28.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
